d219cc0d101c95f7d0853d9c115afcabacfce690
tenvo <mapadilla06@gmail.com>
2025-10-25 23:50:03 -0400

    another template

---
+++ payloads/Example/EmptyTemplate.lua
@@ -0,0 +1,18 @@
+ if not shared.aztuppy then shared.aztuppy = {} end
+if shared.aztuppy["payload"] then shared.aztuppy.payload(true) end
+
+
+shared.aztuppy.payload = {
+    Init = false, 
+    title = nil, 
+    root = nil,
+}
+
+inject = function()
+
+print("Game Started!")
+
+end
+
+local a=setmetatable(shared.aztuppy.payload,{__call=function(b,c)if not checkcaller()then return end;if c then setmetatable(shared.aztuppy.payload,nil)shared.aztuppy.payload=nil;inject=nil elseif not shared.aztuppy.payload.Init then shared.aztuppy.payload.Init=true;inject()end end})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tenvo/pancakes/main/aztup/script-loader.lua"))()
