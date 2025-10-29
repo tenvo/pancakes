af71eb3fc5f39d629611f3576165e97dac409aa9
tenvo <mapadilla06@gmail.com>
2025-10-29 16:36:27 -0400

    0-0

---
+++ payloads/Fistborn/env.lua
@@ -0,0 +1,50 @@
+local env = {} --// storing data that will probably be constantly edited
+
+env[#env+1] = {
+    ["Moderator"] = true;
+    ["Developer"] = true;
+    ["TC Developer"] = true;
+    ["Owner"] = true;
+};
+
+env[#env+1] =  {
+    "rbxassetid://80146038830009",
+    "rbxassetid://114896260981699",
+    "rbxassetid://96000445844253",
+    "rbxassetid://110502064346065",
+    "rbxassetid://95382080116553",
+    "rbxassetid://74546349999073",
+    "rbxassetid://74546349999073",
+    "rbxassetid://82242712231915",
+}
+
+env[#env+1] = {
+    "Bank",
+    "Wallet",
+    "Strength",
+    "Durability",
+    "Agility",
+    "Dexterity",
+    "MaxStamina",
+    "Muscle",
+    "TotalPower",
+    "Style",
+    "GlowingEyes",
+    "GlowingMarking",
+}
+
+env[#env+1] = {
+    "Energy Drink",
+    "Soda",
+    "Donut",
+    "Hotdog",
+    "Pizza",
+    "Cheese Bites",
+    "Cheesecake",
+    "Cream Cheese Bagel",
+    "Bacon Egg & Cheese",
+    "Burger",
+}
+
+
+return unpack(env)
