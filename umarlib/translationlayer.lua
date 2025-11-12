2510a214060cc8947a595f50f1fdbff1aec1402d
tenvo <mapadilla06@gmail.com>
2025-11-12 15:47:08 -0500

    progress

--- umarlib/translationlayer.lua
+++ umarlib/translationlayer.lua
@@ -1,5 +1,97 @@
 local umarlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tenvo/pancakes/main/umarlib/library.lua"))()
 
-local library = {}
+local interpeter = {}
 
-return library
+function interpeter:Init()
+    self = setmetatable({}, interpeter);
+    self.columns = {}
+    self.tabs = {}
+    self.flags = {}
+    self.main = umarlib:Window("pancake fan club <3",Color3.fromRGB(math.random(0,255),math.random(0,255),math.random(0,255)))
+    self.umarlib = true
+
+    --// add metatable to init settings/config
+end
+
+function interpeter:AddTab(name)
+    local Tab = {}
+    local thisTab = self.main:Tab(name)
+
+    self.tabs[name] = thisTab
+
+    function Tab:AddColumn()
+        local Column = {}
+        setmetatable(Column, {__index = function(mytable, key)
+            print(key,"Column Index")
+        end})
+
+        function Column:AddSection(name)
+            local Section = {}
+            Section.Name = name
+            self.main:Label(string.format("-- %s --",name))
+            
+            function Section:AddButton(payload)
+                print(Section.Name,"wants to make a button")
+                print("Payload:")
+                for i,v in pairs(payload) do
+                    print(i,v)
+                end
+                print(" ")
+            end
+
+            function Section:AddToggle(payload)
+                print(Section.Name,"wants to make a Toggle")
+                print("Payload:")
+                for i,v in pairs(payload) do
+                    print(i,v)
+                end
+                print(" ")
+            end
+
+            function Section:AddDivider(name)
+                print(Section.Name,"wants to make a Divider")
+                print(name)
+                print(" ")
+            end
+
+            function Section:AddSlider(payload)
+                print(Section.Name,"wants to make a Slider")
+                print("Payload:")
+                for i,v in pairs(payload) do
+                    print(i,v)
+                end
+                print(" ")
+            end
+
+            function Section:AddList(payload)
+                print(Section.Name,"wants to make a List")
+                print("Payload:")
+                for i,v in pairs(payload) do
+                    print(i,v)
+                end
+                print(" ")
+            end
+
+            function Section:AddBind(payload)
+                print(Section.Name,"wants to make a Bind")
+                print("Payload:")
+                for i,v in pairs(payload) do
+                    print(i,v)
+                end
+                print(" ")
+            end
+
+
+            return Section
+        end
+
+        table.insert(self.columns,thisTab)
+        return Column
+    end
+
+    return Tab
+end
+
+
+interpeter:Init()
+return interpeter
